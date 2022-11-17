c1a4635e9db4a83a1f1dafda0a51311a30a795ec
randabt <benthlija.randa@gmail.com>
2022-11-17 09:29:34 +0100

    add program details fragment to the nav graph add button to fragment_programs.xml to navigate to program details for test

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/ProgramsFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/ProgramsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.peculiaruc.alc_mmsystem_admin.R
 
 class ProgramsFragment : Fragment() {
@@ -15,7 +17,15 @@ class ProgramsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_programs, container, false)
+        //return inflater.inflate(R.layout.fragment_programs, container, false)
+        val root= inflater.inflate(R.layout.fragment_programs, container, false)
+        val bt=root.findViewById<Button>(R.id.programDetailsButton)
+        bt.setOnClickListener {
+            val action =
+                ProgramsFragmentDirections.actionProgramsFragmentToProgramDetailsFragment(1)
+            view?.findNavController()?.navigate(action)
+        }
+        return root
     }
 
 
