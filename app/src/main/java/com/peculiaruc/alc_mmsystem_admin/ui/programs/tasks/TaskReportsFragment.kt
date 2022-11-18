28991b9c71faf37536d8375f01ecbbb045cf2f92
randabt <benthlija.randa@gmail.com>
2022-11-18 15:37:40 +0100

    update and format

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/tasks/TaskReportsFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/tasks/TaskReportsFragment.kt
@@ -11,18 +11,20 @@ import com.peculiaruc.alc_mmsystem_admin.databinding.FragmentTaskReportsBinding
 import com.peculiaruc.alc_mmsystem_admin.domain.models.Report
 import com.peculiaruc.alc_mmsystem_admin.ui.programs.ProgramDetailsViewModel
 
-class TaskReportsFragment :BaseFragment<FragmentTaskReportsBinding>(), TaskReportsAdapter.ItemClickListener{
+class TaskReportsFragment : BaseFragment<FragmentTaskReportsBinding>(),
+    TaskReportsAdapter.ItemClickListener {
 
     override val layoutIdFragment: Int = R.layout.fragment_task_reports
     override val viewModel: TaskReportsViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.initTaskReports()
         val recyclerView = binding.taskReportsList
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         val taskReportsAdapter = TaskReportsAdapter(this)
         recyclerView.adapter = taskReportsAdapter
-        viewModel.taskReports.observe(viewLifecycleOwner){
+        viewModel.taskReports.observe(viewLifecycleOwner) {
             taskReportsAdapter.submitList(it)
         }
     }
