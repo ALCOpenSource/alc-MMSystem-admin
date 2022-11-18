f85316c3cbc1e838780f7c348c3fb322ba1e7f98
randabt <benthlija.randa@gmail.com>
2022-11-18 16:04:31 +0100

    add string array to strings.xml implement spinner

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/tasks/TaskReportsFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/tasks/TaskReportsFragment.kt
@@ -2,14 +2,15 @@ package com.peculiaruc.alc_mmsystem_admin.ui.programs.tasks
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.peculiaruc.alc_mmsystem_admin.R
-import com.peculiaruc.alc_mmsystem_admin.ui.base.BaseFragment
 import com.peculiaruc.alc_mmsystem_admin.databinding.FragmentTaskReportsBinding
 import com.peculiaruc.alc_mmsystem_admin.domain.models.Report
-import com.peculiaruc.alc_mmsystem_admin.ui.programs.ProgramDetailsViewModel
+import com.peculiaruc.alc_mmsystem_admin.ui.base.BaseFragment
 
 class TaskReportsFragment : BaseFragment<FragmentTaskReportsBinding>(),
     TaskReportsAdapter.ItemClickListener {
@@ -17,10 +18,19 @@ class TaskReportsFragment : BaseFragment<FragmentTaskReportsBinding>(),
     override val layoutIdFragment: Int = R.layout.fragment_task_reports
     override val viewModel: TaskReportsViewModel by viewModels()
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var spinner: Spinner
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.initTaskReports()
-        val recyclerView = binding.taskReportsList
+        recyclerView = binding.taskReportsList
+        spinner = binding.taskSpinner
+        setUpRecycleView()
+        setUpSpinner()
+    }
+
+    fun setUpRecycleView() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         val taskReportsAdapter = TaskReportsAdapter(this)
         recyclerView.adapter = taskReportsAdapter
@@ -29,6 +39,18 @@ class TaskReportsFragment : BaseFragment<FragmentTaskReportsBinding>(),
         }
     }
 
+    fun setUpSpinner(){
+        context?.let {
+            val spinnerAdapter = ArrayAdapter.createFromResource(
+                it,
+                R.array.reports_string_array,
+                android.R.layout.simple_spinner_item
+            )
+            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = spinnerAdapter
+
+        }
+    }
     override fun onReportItemClick(item: Report) {
         TODO("Not yet implemented")
     }
