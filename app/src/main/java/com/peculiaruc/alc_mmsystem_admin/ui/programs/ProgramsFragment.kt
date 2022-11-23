504383f8afc59339a7c8489397d4d7f4be2906d0
randabt <benthlija.randa@gmail.com>
2022-11-23 15:38:40 +0100

    create criteria input selection bottom sheet dialog layout add criteria buttons style to themes.xml add test button to fragment_programs.xml to open the bottom sheet dialog

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/ProgramsFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/programs/ProgramsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.peculiaruc.alc_mmsystem_admin.R
 
 class ProgramsFragment : Fragment() {
@@ -24,6 +25,58 @@ class ProgramsFragment : Fragment() {
             val action =
                 ProgramsFragmentDirections.actionProgramsFragmentToProgramDetailsFragment(1)
             view?.findNavController()?.navigate(action)
+        }
+        val criteriaBt = root.findViewById<Button>(R.id.criteriaButton)
+        criteriaBt.setOnClickListener {
+            /*var dialogFragment=CriteriaInputSelectionDialogFragment()
+            dialogFragment.view?.layoutParams= ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
+            dialogFragment.show(childFragmentManager,"")*/
+            // on below line we are creating a new bottom sheet dialog.
+            context?.let { it1 ->
+                val dialog = BottomSheetDialog(it1)
+
+                // on below line we are inflating a layout file which we have created.
+                val view =
+                    layoutInflater.inflate(R.layout.fragment_criteria_input_selection_dialog, null)
+
+                // on below line we are creating a variable for our button
+                // which we are using to dismiss our dialog.
+                val btnCancel = view.findViewById<Button>(R.id.criteriaSelectionCancelButton)
+                val btnSingleInput = view.findViewById<Button>(R.id.singleInputButton)
+                val btnYesNoInput = view.findViewById<Button>(R.id.inputButtonYesNo)
+                val btnFileInput = view.findViewById<Button>(R.id.inputButtonFile)
+                val btnMultipleInput = view.findViewById<Button>(R.id.inputButtonMultiple)
+                val btnMultiChoiceInput = view.findViewById<Button>(R.id.inputButtonMultiChoice)
+
+                btnCancel.setOnClickListener {
+                    dialog.dismiss()
+                }
+                btnSingleInput.setOnClickListener {
+
+                }
+                btnYesNoInput.setOnClickListener {
+
+                }
+                btnFileInput.setOnClickListener {
+
+                }
+                btnMultipleInput.setOnClickListener {
+
+                }
+                btnMultiChoiceInput.setOnClickListener {
+
+                }
+
+                dialog.setCancelable(false)
+
+                // on below line we are setting
+                // content view to our view.
+                dialog.setContentView(view)
+
+                // on below line we are calling
+                // a show method to display a dialog.
+                dialog.show()
+            }
         }
         return root
     }
