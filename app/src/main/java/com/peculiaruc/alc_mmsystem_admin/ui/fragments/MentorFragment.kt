d75e514c4844555fddf6b3e384d001e6eeedc013
davadeola <adeoladavid15@gmail.com>
2022-11-28 08:51:18 +0300

    implemented mentor dropdown

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/fragments/MentorFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/fragments/MentorFragment.kt
@@ -2,6 +2,8 @@ package com.peculiaruc.alc_mmsystem_admin.ui.fragments
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,9 +35,19 @@ class MentorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            //setup tool bar
             mentorToolbar.inflateMenu(R.menu.mentor_list_menu)
+            val searchItem = mentorToolbar.menu.findItem(R.id.search)
+            val searchView = searchItem.actionView as SearchView
+            searchView.queryHint = "Samuel Kabede"
+            searchView.isIconified = false
             mentorToolbar.setNavigationIcon(R.drawable.arrow_left)
             mentorToolbar.title = "Mentors"
+
+            //setup dropdown
+            val mentorStates = resources.getStringArray(R.array.mentor_dropdown_text)
+            val arrayAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown, mentorStates)
+            autoCompleteMentor.setAdapter(arrayAdapter)
 
             fabAddMentor.setOnClickListener {
                 findNavController().navigate(MentorFragmentDirections.actionMentorFragmentToAddMentorDialog())
@@ -50,10 +62,14 @@ class MentorFragment : Fragment() {
         val mentorListAdapter = MentorListAdapter()
 
         recyclerView.adapter = mentorListAdapter
-
-
-
     }
 
-
+//    @Deprecated("Deprecated in Java")
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.mentor_list_menu, menu)
+//
+//        val searchItem = menu.findItem(R.id.search)
+//        val searchView = searchItem.actionView as SearchView
+//        searchView.queryHint ="Samuel Kabede"
+//    }
 }
