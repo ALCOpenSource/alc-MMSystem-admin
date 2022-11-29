7bd2527b7831ef4e451994b08aa2a807f53ec12d
David <ndekeredavid@gmail.com>
2022-11-29 15:29:42 +0300

    Added report doc

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/domain/models/Report.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/domain/models/Report.kt
@@ -1,4 +1,6 @@
 package com.peculiaruc.alc_mmsystem_admin.domain.models
 
-data class Report (val id:Int, val title: String, val time: String, val user: String) {
-}
+/*
+* Report class
+* */
+data class Report(val id: Int, val title: String, val time: String, val user: String)
